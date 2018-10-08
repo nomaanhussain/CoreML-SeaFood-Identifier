be8dd8853bf0c17a33abb39619e8b41df12cc750
Noman Hussain <nomanghanchi_11@yahoo.com>
2018-10-08 20:14:08 +0500

    Changes

---
+++ SeeFood-CoreML/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  SeeFood-CoreML
+//
+//  Created by Noman Hussain on 27/08/2018.
+//  Copyright © 2017 Noman Hussain. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    var classificationResults : [VNClassificationObservation] = []
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        // Load the ML model through its generated class
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("can't load ML model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation],
+                let topResult = results.first
+                else {
+                    fatalError("unexpected result type from VNCoreMLRequest")
+                }
+            
+                if topResult.identifier.contains("hotdog") {
+                    DispatchQueue.main.async {
+                        self.navigationItem.title = "Hotdog!"
+                        self.navigationController?.navigationBar.barTintColor = UIColor.green
+                        self.navigationController?.navigationBar.isTranslucent = false
+                        
+                        
+                    }
+                }
+                else {
+                    DispatchQueue.main.async {
+                        self.navigationItem.title = "Not Hotdog!"
+                        self.navigationController?.navigationBar.barTintColor = UIColor.red
+                        self.navigationController?.navigationBar.isTranslucent = false
+                        
+                    }
+                }
+                
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do { try handler.perform([request]) }
+        catch { print(error) }
+        
+        
+        
+    }
+    
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+                    
+            imageView.image = image
+            
+            imagePicker.dismiss(animated: true, completion: nil)
+            
+            
+            guard let ciImage = CIImage(image: image) else {
+                fatalError("couldn't convert uiimage to CIImage")
+            }
+            
+            detect(image: ciImage)
+            
+        }
+    }
+        
+
+    @IBAction func cameraTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+   
+}
+
